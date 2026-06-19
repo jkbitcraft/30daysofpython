9a363d594132ec06ed1b240df457662feb1937bb
Jun <ericguotu@gmail.com>
2026-06-19 23:43:09 +1000

    feat: Exercises level 1

---
+++ day_2/variables.py
@@ -0,0 +1,14 @@
+#Exercises: Level 1
+#Day 2: 30 Days of python programming
+first_name = "Eric"
+last_name = "Lastname"
+full_name = f"{first_name} {last_name}"
+country = "Australia"
+city = "Sydney"
+age = 200
+year = 2026
+is_married = True
+is_true = False
+is_light = True
+middle_name, middle_age, middle_city = "Hello", "190", "World"
+
