662f63db2286a53b4f35950fa597b0937cc0440e
Jun <ericguotu@gmail.com>
2026-06-20 00:13:23 +1000

    feat: variables.py. Exercises Level 1 and 2 + README updates for what was done

--- day_2/README.md
+++ day_2/README.md
@@ -11,3 +11,14 @@ Learned:
 - Data types and casting
 
 Exercise: Level 1
+
+- Created day_2 folder -> variables.py file
+- Added comment
+- Declared range of variables
+
+Exercise: Level 2
+
+- Used built-in functions
+- Simple arithmetic
+- Input functions
+- Python shell help('keywords')
--- day_2/variables.py
+++ day_2/variables.py
@@ -1,3 +1,5 @@
+import math
+
 #Exercises: Level 1
 #Day 2: 30 Days of python programming
 first_name = "Eric"
@@ -12,3 +14,35 @@ is_true = False
 is_light = True
 middle_name, middle_age, middle_city = "Hello", "190", "World"
 
+#Exercises: Level 2
+type(first_name)
+type(last_name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+type(is_light)
+type(middle_name, middle_age, middle_city)
+
+len(first_name)
+print(len(first_name)-len(last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two*num_one
+division = num_one/num_two
+remainder = num_two%num_one
+exp = num_one**num_two
+floor_division = num_one//num_two
+area_of_circle = (math.pi*30)**2
+circum_of_circle = 2*math.pi*30
+radius = input("Input Radius: ")
+area_of_input_circle = (math.pi*radius)**2
+input_first_name = input("First name: ")
+input_last_name = input("Last name: ")
+input_country = input("Country: ")
+input_age = input("Age: ")
